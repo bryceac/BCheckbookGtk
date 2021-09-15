8249146bf86d2c74b68ae2e01c0aa7f36893f898
Bryce Campbell <tonyhawk2100@gmail.com>
2021-09-15 14:38:29 -0700

    add comment attributing source.

--- Sources/bcheckbook/Extensions/WidgetProtocol+apply.swift
+++ Sources/bcheckbook/Extensions/WidgetProtocol+apply.swift
@@ -1,5 +1,9 @@
 import Gtk
 
+/*
+extension to add synatic sugar for UI, as implemented by Mikolasstuchlik at the following address:
+https://github.com/mikolasstuchlik/BurningRingOfFire/blob/master/Sources/BurningRingOfFire/global/Widget%2BExtensions.swift
+*/
 extension WidgetProtocol {
     public func apple(_ block: (Self) -> Void) -> Self {
         block(self)
