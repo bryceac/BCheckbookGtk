39d193561ff56f8db984b371dfa725c1c9f1d1cc
Bryce Campbell <tonyhawk2100@gmail.com>
2022-01-15 15:12:48 -0800

    work on adding a database backend.

--- Package.swift
+++ Package.swift
@@ -8,13 +8,16 @@ let package = Package(
     dependencies: [
         .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("main")),
         .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("gtk3")),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.13.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "bcheckbook",
-            dependencies: ["Gtk"],
+            dependencies: ["Gtk",
+            .product(name: "SQLite", package: "SQLite.swift")
+            ],
             resources: [
                 .process("Resources/window.ui"),
                 .process("Resources/menus.ui"),
--- Sources/bcheckbook/Models/Windows/MainWindow.swift
+++ Sources/bcheckbook/Models/Windows/MainWindow.swift
@@ -4,28 +4,29 @@ import GLibObject
 import Foundation
 
 class MainWindow: WindowModel {
-    var builder: Builder? = Builder("window")
+    let records = Records()
 
     lazy var store = ListStore(builder?.get("store", Gtk.ListStoreRef.init).list_store_ptr)!
 
     let iterator: TreeIter = TreeIter()
+    let store = ListStore(.string, .string, .boolean, .string, .string, .string, .string, .string)
+    let listView = ListView(model: store)
 
-    lazy var scrollView = builder?.get("scrollView", ScrolledWindowRef.init)
+    lazy var scrollView = ScrolledWindow()
 
-    // retrieve cell renderers, so that data can be manipulated inside tree view.
-    lazy var dateCell = builder?.get("dateCellRenderer", CellRendererTextRef.init)
-
-    lazy var checkNumberCell = builder?.get("checkNumberCellRenderer", CellRendererTextRef.init)
-
-    lazy var isReconciledCell = builder?.get("reconciledCellRenderer", CellRendererToggleRef.init)
-
-    lazy var vendorCell = builder?.get("vendorCellRenderer", CellRendererTextRef.init)
-
-    lazy var memoCell = builder?.get("memoCellRenderer", CellRendererTextRef.init)
-
-    lazy var depositCell = builder?.get("depositCellRenderer", CellRendererTextRef.init)
-
-    lazy var withdrawalCell = builder?.get("withdrawalCellRenderer", CellRendererTextRef.init)
+    let columns = [
+        ("Date", "text", CellRendererText()),
+        ("Check #", "text", CellRendererText()),
+        ("Reconciled", "active", CellRendererToggle()),
+        ("Vendor", "text", CellRendererText()),
+        ("Memo", "text", CellRendererText()),
+        ("Category", "text", CellRendererText()),
+        ("Deposit", "text", CellRendererText()),
+        ("Withdrawal", "text", CellRendererText()),
+        ("Balance", "text", CellRendererText())
+    ].enumerated().map {(i: Int, c:(title: String, kind: PropertyName, renderer: CellRenderer)) in
+        TreeViewColumn(i, title: c.title, renderer: c.renderer, attribute: c.kind)
+    }
 
     var application: ApplicationRef? = nil
 
@@ -37,133 +38,14 @@ class MainWindow: WindowModel {
             updateViews()
         }
     }
- 
-    // create property to house the transactions
-    let records = Records.shared
 
     override func make(window: Gtk.Window) {
         super.make(window: window)
 
-        window.title = "Hello, World!"
+        window.title = "BCheckbook"
         window.setDefaultSize(width: 800, height: 600)
 
-        var accelGroup: AccelGroup! = {
-            let group = AccelGroupRef().link(to: AccelGroup.self)!
-            return group
-        }()
-
-        window.add(accelGroup: accelGroup)
-
-        var quitItem: MenuItem! = MenuItemRef(label: "Quit").link(to: MenuItem.self)?.apply { item in
-            item.addAccelerator(accelSignal: "activate",
-            accelGroup: accelGroup,
-            accelKey: Int(Gdk.KEY_q),
-            accelMods: ModifierType.controlMask,
-            accelFlags: AccelFlags.visible)
-
-            item.onActivate { [weak self] _ in
-                self?.application?.quit()
-            }
-        }
-
-        var fileMenu: Menu! = MenuRef().link(to: Menu.self)?.apply { menu in
-            menu.append(child: quitItem)
-        }
-
-        var fileItem: MenuItem! = MenuItemRef(label: "File").link(to: MenuItem.self)?.apply { item in
-            item.set(submenu: fileMenu)
-        }
-
-        var menuBar: MenuBar! = MenuBarRef().link(to: MenuBar.self)?.apply { bar in
-            bar.append(child: fileItem)    
-        }
-
-        self.application?.set(menubar: menuBar)
-
-        checkNumberCell?.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
-            let path = TreePath(string: path)
-
-            let RECORD_ID = self.records.sortedRecords[path.index].id
-            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
-
-            record.event.checkNumber = Int(newValue) 
-
-            self.updateViews()
-        }
-
-        // make sure data is modified appropriately for each cell
-        dateCell?.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
-            let path = TreePath(string: path)
-
-            guard let newDate = Event.DF.date(from: newValue) else { return } 
-
-            let RECORD_ID = self.records.sortedRecords[path.index].id
-            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
-
-            record.event.date = newDate
-
-            self.updateViews()
-        }
-
-        isReconciledCell?.onToggled { [weak self] _, string in
-            let path = TreePath(string: string)
-
-            let RECORD_ID = self?.records.sortedRecords[path.index].id
-            guard let record = self?.records.items.first(where: { $0.id == RECORD_ID }) else { return }
-            record.event.isReconciled.toggle()
-
-            self?.updateViews()
-        }
-
-        vendorCell?.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
-            let path = TreePath(string: path)
-
-            let RECORD_ID = self.records.sortedRecords[path.index].id
-            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
-
-            record.event.vendor = newValue
-
-            self.updateViews()
-        }
-
-        memoCell?.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
-            let path = TreePath(string: path)
-
-            let RECORD_ID = self.records.sortedRecords[path.index].id
-            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
-
-            record.event.memo = newValue
-
-            self.updateViews()
-        }
-
-        depositCell?.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
-            let path = TreePath(string: path)
-
-            guard let amount = Double(newValue) else { return }
-
-            let RECORD_ID = self.records.sortedRecords[path.index].id
-            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
-
-            record.event.amount = amount
-            record.event.type = .deposit
-
-            self.updateViews()
-        }
-
-        withdrawalCell?.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
-            let path = TreePath(string: path)
-
-            guard let amount = Double(newValue) else { return }
-
-            let RECORD_ID = self.records.sortedRecords[path.index].id
-            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
-
-            record.event.amount = amount
-            record.event.type = .withdrawal
-            
-            self.updateViews()
-        }
+        
         window.add(widget: scrollView!)
     }
 
