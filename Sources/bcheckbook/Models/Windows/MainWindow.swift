89f7bcb4cbfd921473134f9b53e86c43c3d5cc99
Bryce Campbell <tonyhawk2100@gmail.com>
2022-01-15 20:32:41 -0800

    start work on trying to allow data to be edited.

--- Sources/bcheckbook/Models/Windows/MainWindow.swift
+++ Sources/bcheckbook/Models/Windows/MainWindow.swift
@@ -8,23 +8,18 @@ class MainWindow: WindowModel {
 
     let iterator: TreeIter = TreeIter()
     let store = ListStore(.string, .string, .boolean, .string, .string, .string, .string, .string, .string)
+
+    let dateCell = CellRendererText()
+    let checkNumberCell = CellRendererText()
+    let reconciledCell = CellRendererToggle()
+    let vendorCell = CellRendererText()
+    let memoCell = CellRendererText()
+    let categoryCell = CellRendererText()
+    let depositCell = CellRendererText()
+    let withdrawalCell = CellRendererText()
     
 
     var scrollView = ScrolledWindow()
-
-    let columns = [
-        ("Date", "text", CellRendererText()),
-        ("Check #", "text", CellRendererText()),
-        ("Reconciled", "active", CellRendererToggle()),
-        ("Vendor", "text", CellRendererText()),
-        ("Memo", "text", CellRendererText()),
-        ("Category", "text", CellRendererText()),
-        ("Deposit", "text", CellRendererText()),
-        ("Withdrawal", "text", CellRendererText()),
-        ("Balance", "text", CellRendererText())
-    ].enumerated().map {(i: Int, c:(title: String, kind: PropertyName, renderer: CellRenderer)) in
-        TreeViewColumn(i, title: c.title, renderer: c.renderer, attribute: c.kind)
-    }
 
     var application: ApplicationRef? = nil
 
@@ -43,8 +38,125 @@ class MainWindow: WindowModel {
         window.title = "BCheckbook"
         window.setDefaultSize(width: 800, height: 600)
 
+        let columns = [
+        ("Date", "text", dateCell),
+        ("Check #", "text", checkNumberCell),
+        ("Reconciled", "active", reconciledCell),
+        ("Vendor", "text", vendorCell),
+        ("Memo", "text", memoCell),
+        ("Category", "text", categoryCell),
+        ("Deposit", "text", depositCell),
+        ("Withdrawal", "text", withdrawalCell),
+        ("Balance", "text", CellRendererText())
+        ].enumerated().map {(i: Int, c:(title: String, kind: PropertyName, renderer: CellRenderer)) in
+            TreeViewColumn(i, title: c.title, renderer: c.renderer, attribute: c.kind)
+        }
+
         let listView = ListView(model: store)
         listView.append(columns)
+
+        dateCell.set(property: 
+        .editable, value: true)
+
+        dateCell.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
+            let path = TreePath(string: path)
+
+            guard let newDate = Event.DF.date(from: newValue) else { return } 
+
+            let RECORD_ID = self.records.sortedRecords[path.index].id
+            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
+
+            record.event.date = newDate
+
+            self.updateViews()
+        }
+
+        checkNumberCell.set(property: .editable, value: true)
+
+        checkNumberCell.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
+            let path = TreePath(string: path)
+
+            let newCheckNumber = Int(newValue)
+
+            let RECORD_ID = self.records.sortedRecords[path.index].id
+            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
+
+            record.event.checkNumber = newCheckNumber
+
+            self.updateViews()
+        }
+
+        reconciledCell.set(property: .activatable, value: true)
+
+        reconciledCell.onToggled { [weak self] _, string in
+            let path = TreePath(string: string)
+
+            let RECORD_ID = self?.records.sortedRecords[path.index].id
+            guard let record = self?.records.items.first(where: { $0.id == RECORD_ID }) else { return }
+            record.event.isReconciled.toggle()
+
+            self?.updateViews()
+        }
+
+        vendorCell.set(property: 
+        .editable, value: true)
+
+        vendorCell.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
+            let path = TreePath(string: path) 
+
+            let RECORD_ID = self.records.sortedRecords[path.index].id
+            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
+
+            record.event.vendor = newValue
+
+            self.updateViews()
+        }
+
+        memoCell.set(property: .editable, value: true)
+
+        memoCell.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
+            let path = TreePath(string: path)
+
+            let RECORD_ID = self.records.sortedRecords[path.index].id
+            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
+
+            record.event.memo = newValue
+
+            self.updateViews()
+        }
+
+        categoryCell.set(property: 
+        .editable, value: true)
+
+        categoryCell.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
+            let path = TreePath(string: path) 
+
+            let RECORD_ID = self.records.sortedRecords[path.index].id
+            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
+
+            record.event.category = !newValue.isEmpty ?  newValue : nil
+
+            self.updateViews()
+        }
+
+        depositCell.set(property: .editable, value: true)
+
+        depositCell.onEdited { (_ unOwnedSelf: CellRendererTextRef, _ path: String, _ newValue: String) in
+            let path = TreePath(string: path)
+
+            let RECORD_ID = self.records.sortedRecords[path.index].id
+            guard let record = self.records.items.first(where: { $0.id == RECORD_ID }) else { return }
+
+            if let newAmount = Double(newValue) {
+                record.event.amount = newAmount
+            } else if let amountNumber = Event.CURRENCY_FORMAT.number(from: newValue) {
+                record.event.amount = amountNumber.doubleValue
+            }
+
+            record.event.type = .deposit
+            
+            self.updateViews()
+        }
 
         scrollView.addWithViewport(child: listView)
 
