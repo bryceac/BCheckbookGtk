6a68d39fb3c83305dd2ca54484b557830acacebc
Bryce Campbell <tonyhawk2100@gmail.com>
2022-01-15 16:17:49 -0800

    update code to find database.

--- Sources/bcheckbook/DB.swift
+++ Sources/bcheckbook/DB.swift
@@ -19,7 +19,6 @@ class DB {
     
     private init() {
         
-        #if os(iOS)
         let DOCUMENTS_DIRECTORY = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         
         url = DOCUMENTS_DIRECTORY.appendingPathComponent("register").appendingPathExtension("db")
@@ -27,19 +26,6 @@ class DB {
         if !FileManager.default.fileExists(atPath: url.absoluteString), let BUNDLE_PATH = Bundle.main.url(forResource: url.deletingPathExtension().lastPathComponent, withExtension: url.pathExtension) {
             try? FileManager.default.copyItem(at: BUNDLE_PATH, to: url)
         }
-        #else
-        let APPLICATION_SUPPORT_DIRECTORY = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
-        
-        let SUPPORT_SUB_DIRECTORY = APPLICATION_SUPPORT_DIRECTORY.appendingPathComponent("me.brycecampbell.bcheckbook")
-        
-        try? FileManager.default.createDirectory(at: SUPPORT_SUB_DIRECTORY, withIntermediateDirectories: true, attributes: nil)
-        
-        url = SUPPORT_SUB_DIRECTORY.appendingPathComponent("register").appendingPathExtension("db")
-        
-        if !FileManager.default.fileExists(atPath: url.absoluteString), let BUNDLE_PATH = Bundle.main.url(forResource: url.deletingPathExtension().lastPathComponent, withExtension: url.pathExtension) {
-            try? FileManager.default.copyItem(at: BUNDLE_PATH, to: url)
-        }
-        #endif
         
         manager = try? DBManager(withDB: url)
     }
