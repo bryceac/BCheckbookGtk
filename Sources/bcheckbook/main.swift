d95bcbd3976dbf94bdd2e82873d4062e10cd3f49
Bryce Campbell <tonyhawk2100@gmail.com>
2022-01-17 16:47:22 -0800

    modify main file, to get things working right.

--- Sources/bcheckbook/main.swift
+++ Sources/bcheckbook/main.swift
@@ -5,23 +5,9 @@ import GLib
 import GIO
 import Foundation
 
-let HOME_DIRECTORY = FileManager.default.homeDirectoryForCurrentUser
-
-/* var appActionEntries = [
-    GActionEntry(name: g_strdup("quit"), activate: { Gtk.ApplicationRef(gpointer: $2).quit() }, parameter_type: nil, state: nil, change_state: nil, padding: (0, 0, 0))
-] */
-
-let status = Application.run(startupHandler: { app in
-    /* if let builder = Builder("menus") {
-        app.menubar = builder.get("menuBar", MenuModelRef.init)
-    } */
-}) { app in
-    
-    // app.addAction(entries: &appActionEntries, nEntries: appActionEntries.count, userData: app.ptr)
-    
+let status = Application.run() { app in
     let main = MainWindow(window: ApplicationWindow(application: app))
     main.application = app
-    main.fileURL = HOME_DIRECTORY.appendingPathComponent("transactions").appendingPathExtension("bcheck")
 
     main.run()
 }
