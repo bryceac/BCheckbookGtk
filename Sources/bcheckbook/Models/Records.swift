b0d61e45146d28f6d691acb55538fe0d74555ff1
Bryce Campbell <tonyhawk2100@gmail.com>
2022-01-24 11:09:42 -0800

    add function to perform filering.

--- Sources/bcheckbook/Models/Records.swift
+++ Sources/bcheckbook/Models/Records.swift
@@ -48,4 +48,40 @@ class Records {
     func clear() {
         items.removeAll()
     }
+
+    func filter(vendor: String? = nil, category: String? = nil) -> [Record] {
+        var filteredRecords: [Record] = sortedRecords
+        
+        switch (vendor, category) {
+        case let (.some(vendor), .some(category)):
+            filteredRecords = filteredRecords.filter { record in
+                if category.caseInsensitiveCompare("Uncategorized") == .orderedSame {
+                    return (record.event.vendor.lowercased().contains(vendor.lowercased()) || record.event.vendor.caseInsensitiveCompare(vendor) == .orderedSame) &&
+                        .none ~= record.event.category
+                } else {
+                    guard let recordCategory = record.event.category else { return false }
+                    
+                    return (record.event.vendor.lowercased().contains(vendor.lowercased()) || record.event.vendor.caseInsensitiveCompare(vendor) == .orderedSame) &&
+                    (recordCategory.lowercased().contains(category.lowercased()) || recordCategory.caseInsensitiveCompare(category) == .orderedSame)
+                }
+                
+            }
+        case let (.some(vendor), .none):
+            filteredRecords = filteredRecords.filter { $0.event.vendor.lowercased().contains(vendor.lowercased()) || $0.event.vendor.caseInsensitiveCompare(vendor) == .orderedSame }
+        case let (.none, .some(category)):
+            filteredRecords = filteredRecords.filter {
+                
+                if category.caseInsensitiveCompare("Uncategorized") == .orderedSame {
+                    return .none ~= $0.event.category
+                } else {
+                    guard let recordCategory = $0.event.category else { return false }
+                    
+                    return recordCategory.lowercased().contains(category.lowercased()) || recordCategory.caseInsensitiveCompare(category) == .orderedSame
+                }
+            }
+        default: ()
+        }
+        
+        return filteredRecords
+    }
 }
